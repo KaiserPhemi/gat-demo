250cd6163aae9efa93395a1b99f0067dd90ada3a
KaiserPhemi <kaiser.phemi@protonmail.ch>
2021-03-24 17:49:15 +0100

    chore()
    - clean up directory

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `My Gatsby Demo`,
+    description: `Just a start`,
+    author: `@KaiserPhemi`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
